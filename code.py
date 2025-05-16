a2424f3076bb240926fadcc8c347884f8bc1eeea
Matteo Bianchi <bmatteo272@gmail.com>
2025-05-16 11:59:59 +0200

    Update code.py

--- code.py
+++ code.py
@@ -11,12 +11,10 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
@@ -24,6 +22,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
+from joblib import dump, load
+import os
 
 
 
@@ -32,6 +32,7 @@ def main():
     preprocessor, X, y, enc = train_features(train_df)
     pipeline = model(preprocessor, X, y)
     test_df = test_features(test_df)
+    validation(pipeline, X, y)
     prediction(test_df, pipeline, enc)
     
     
@@ -97,19 +98,34 @@ def model(preprocessor, X, y):
         },
         {
             'model': [XGBClassifier()],
-            'model__n_estimators': [20, 60],
-            'model__max_depth': [None, 10]
+            'model__n_estimators': [20, 60, 80],
+            'model__max_depth': [None, 10, 20]
         }
     ]
     
-    grid_search = GridSearchCV(pipeline, param_grid, cv=3, scoring='f1_macro', n_jobs=-1)
+    grid_search = GridSearchCV(pipeline, param_grid, cv=4, scoring='f1_macro', n_jobs=-1)
     grid_search.fit(X, y)
     
-    print("Best Model:", type(grid_search.best_estimator_.named_steps['model']).__name__)
+    best_model = grid_search.best_estimator_.named_steps['model']
+    print("Best Model:", type(best_model))
+    print("Best n_estimators:", getattr(best_model, 'n_estimators', 'N/A'))
+    print("Best max_depth:", getattr(best_model, 'max_depth', 'N/A'))
     print("Best F1:", round(grid_search.best_score_, 2))
 
     best_pipeline = grid_search.best_estimator_
+    
+    folder = os.curdir
+    filename = os.path.join(folder,"bestmodel.joblib")
+    dump(best_pipeline, filename=filename)
+    
     return best_pipeline
+
+
+def validation(best_pipeline, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+    print(f"Validation Set:\n{classification_report(y_val, best_pipeline.predict(X_val))}")
+    print(f"Test Set:\n{classification_report(y_test, best_pipeline.predict(X_test))}")
     
     
 def test_features(test_df):
@@ -126,7 +142,6 @@ def test_features(test_df):
     test_df = test_df.loc[test_df["Delay_from_due_date"] >= 0,:]
     test_df = test_df.loc[test_df["Num_of_Delayed_Payment"] >= 0,:]
     test_df = test_df.loc[test_df["Changed_Credit_Limit"] >= 0,:]
-    
     return test_df
     
 def prediction(test_df, pipeline, enc):
@@ -134,6 +149,10 @@ def prediction(test_df, pipeline, enc):
     y_pred = pipeline.predict(test_df)
     y_pred = enc.inverse_transform(y_pred)
     print(pd.Series(y_pred).value_counts(normalize=True))
+    df_final = pd.concat([test_df, pd.Series(y_pred, name="Predicted")], axis=1)
+    folder = os.curdir
+    namefile = os.path.join(folder, "final_df.csv")
+    df_final.to_csv(namefile)
     
     
 if __name__=="__main__":
